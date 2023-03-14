0992846582d40d817499fe7584fe297e60a6e51b
LucianoMarello <lucianomarello27@gmail.com>
2023-03-13 22:35:45 -0300

    Mejora en el js
    Agrega objetos, arrays y mejora el programa en general

--- js/script.js
+++ js/script.js
@@ -1,41 +1,126 @@
-let producto
-let total = 0
-let condicion = prompt('Hola, quiere realizar una compra? Si o No');
-
-while (condicion != 'No') {
-    if (condicion == 'Si') {
-        producto = prompt('Productos disponibles:\nArroz a 220$ x 1kg \nFideos a 500$ x 1kg \nGranola a 350$ x 1kg \nMani a 80$ x 1kg \n\n Ingrese la inicial del producto que quiere comprar (A, F, G o M).');
-        if (producto == 'A'||producto == 'F'||producto == 'G'||producto == 'M') {
-            total=sumar(total, producto)
-            alert('El total de su compra por el momento es de ' + total + '$')
-            condicion = prompt('Quiere seguir comprando? Si o No')
-        } else { alert('Codigo no valido. Escriba A, F, G o M') }
+// Definimos los productos disponibles
+const productos = [
+    {
+        id: 1,
+        nombre: "Arroz",
+        precio: 220,
+        cantidad: 0
+    },
+    {
+        id: 2,
+        nombre: "Fideos",
+        precio: 350,
+        cantidad: 0
+    },
+    {
+        id: 3,
+        nombre: "Granola",
+        precio: 500,
+        cantidad: 0
+    },
+    {
+        id: 4,
+        nombre: "Mani",
+        precio: 160,
+        cantidad: 0
     }
-    else {
-        condicion = prompt('Error. Escriba Si o No')
+];
+
+// Creamos el carrito vacío
+const carrito = [];
+
+// Crea una lista de los productos
+function enlistarProductos() {
+    let mensaje = "PRODUCTOS DISPONIBLES: \n"
+    for (const producto of productos) {
+        mensaje += "ID: " + producto.id + " - Producto: " + producto.nombre + " - Precio: " + producto.precio + "$ x kg \n";
+    }
+    return mensaje;
+}
+
+// Función para mostrar los productos en el carrito
+function mostrarCarrito() {
+    let mensaje = "PRODUCTOS EN EL CARRITO: ";
+    if (carrito.length != 0) {
+        for (const producto of carrito) {
+            mensaje += "\n -" + producto.nombre + "  x " + producto.cantidad + " kilo/s";
+        }
+        alert(mensaje);
+    } else {
+        alert("El carrito esta vacío");
     }
 }
 
-alert('El total a pagar es de '+ total +'$\n\nGracias por su compra.')
-
-
-function sumar(total, producto) {
-    let precio = 0
-    let cantidad = parseInt(prompt('Cuanto kg quiere comprar?'))
-    switch (producto) {
-        case 'A':
-            precio = 220
-            break;
-        case 'F':
-            precio = 500
-            break;
-        case 'G':
-            precio = 350
-            break;
-        case 'M':
-            precio = 80
-            break;
+// Función para calcular el total de la compra
+function calcularTotal(carrito) {
+    let total = 0;
+    for (const producto of carrito) {
+        total += (producto.precio*producto.cantidad);
     }
-    total = total + (precio * cantidad)
-    return(total);
+    return total;
+}
+
+// Pregunta que productos quiere comprar el usuario
+let seguirComprando = true;
+let lista = enlistarProductos();
+while (seguirComprando) {
+    let idProducto = parseInt(prompt(lista + "\nIngrese el ID del producto que desea agregar al carrito (0 para finalizar):"));
+    if (idProducto === 0) {
+        seguirComprando = false;
+    } else {
+        const productoEncontrado = productos.find(producto => producto.id === idProducto);
+        // Si el ID ingresado coincide con algún producto, se pregunta la cantidad a comprar y luego se agrega al carrito
+        if (productoEncontrado) {
+            let cantidad = parseInt(prompt("Cuantos kilos de " + productoEncontrado.nombre + " quiere comprar?"));
+            if (isNaN(cantidad)) {
+                alert("Error, ingrese un número.");
+            } else if(cantidad!=0){
+                // Agrega el producto al carrito
+                let encontrado = false;
+                for (i = 0; i < carrito.length; i++) {
+                    // Verifica si el producto ya está incluido en el carrito
+                    if (carrito[i].id === productoEncontrado.id) {
+                        encontrado = true;
+                        break;
+                    }
+                }
+                if (encontrado) {
+                    // Si el producto ya está en el carrito, se le suma la cantidad de kilos deseada
+                    alert("Habia " + carrito[i].cantidad + " kilo/s de " + productoEncontrado.nombre + " y ahora se agregaron " + cantidad + " kilo/s");
+                    carrito[i].cantidad = carrito[i].cantidad + cantidad;
+                    mostrarCarrito();
+                } else { // Si no, se agrega el producto al carrito
+                    alert("Se agregaron " + cantidad + " kilo/s de " + productoEncontrado.nombre);
+                    productoEncontrado.cantidad = cantidad;
+                    carrito.push(productoEncontrado);
+                    mostrarCarrito();
+                }
+            }
+        } else {
+            // Si el ID no coincide con ningún producto, se informa al usuario y vuelva al principio del while
+            alert("No se encontró ningún producto con el ID ingresado.");
+        }
+    }
+}
+
+// Mostramos los productos del carrito al finalizar
+mostrarCarrito();
+
+// Verificamos si hay algún producto en el carrito y llamamos a la función para calcular el total si hace falta
+if(carrito.length!=0){
+    let total = calcularTotal(carrito);
+    let control = true
+    do{
+        let confirmar = prompt("El total de la compra es de "+total+"$ \n\nPara confirmar su compra escriba CONFIRMAR \nPara cancelarla escriba CANCELAR");
+        //Pedimos confirmación de la compra
+        if(confirmar.toUpperCase()==="CONFIRMAR"){
+            alert("Su pedido ha sido confirmardo. Muchas gracias por su compra!");
+            control=false;
+        }else if(confirmar.toUpperCase()==="CANCELAR"){
+            alert("Su compra ha sido cancelada.");
+            control=false;
+        }else{
+            alert("Por favor, escriba CONFIRMAR o CANCELAR");
+        }
+    }while(control)
 }
